246eeb5140a19840a0b7215b5cbe1414e17eb4bd
CodingWill0 <38038438+CodingWill0@users.noreply.github.com>
2020-04-08 17:41:19 +0200

    Añadido caso sin vuelos

--- Web Scrapping/aeropuertos-espana.py
+++ Web Scrapping/aeropuertos-espana.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 lista = []
 
 def links():   
-
     page = requests.get("https://aeropuertosdelmundo.net/europa/espana")
     soup = BeautifulSoup(page.content, 'html.parser')
     aeropuertos = soup.select(".btn.btn-md.btn-ranking")
@@ -16,14 +15,11 @@ def links():
         # print(link)
         pag = requests.get(link)
         unico = BeautifulSoup(pag.content, 'html.parser')
-        
         salidas_link = unico.select('div.card-header ol li:nth-of-type(2) a')
         # print(salidas_link[0].get('href'))
-        
         link_salidas = "https://aeropuertosdelmundo.net"+ salidas_link[0].get('href')
         pagina = requests.get(link_salidas)
         html = BeautifulSoup(pagina.content, 'html.parser')
-        
         frame = html.select('iframe')
         elem = frame[0].get('src')
         lista.append(elem)
@@ -31,89 +27,97 @@ def links():
 if __name__ == "__main__":   
     links()
       
-
 async def main():
 
     for link in lista: 
-        
         browser = await launch()
         page = await browser.newPage()
         await page.goto(link)
-        await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value=1; document.querySelector("#flightStatusByRouteForm").submit()}')
-        await page.waitForNavigation()
-        id_vuelos = []
+        noSelect = await page.evaluate('() => document.querySelector("#flightStatusByRouteForm > select") == null')
+        #print(noSelect)
+        if(noSelect == True):
+            id_vuelos = []
+            soup = BeautifulSoup(await page.content(), 'html.parser')
+            tabla = soup.select(".tableListingTable > tbody > tr")
+            tabla.pop(0)
+            for fila in tabla:
+                id_vuelos.append(fila.select_one("td > a")['href'])
+        else:
+            await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value=1; document.querySelector("#flightStatusByRouteForm").submit()}')
+            await page.waitForNavigation()
+            id_vuelos = []
+            
+            #Select 1
+            soup = BeautifulSoup(await page.content(), 'html.parser')
+            tabla = soup.select(".tableListingTable > tbody > tr")
+            tabla.pop(0)
+            for fila in tabla:
+                id_vuelos.append(fila.select_one("td > a")['href'])
+            await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
+            await page.waitForNavigation()
 
-        #Select 1
-        soup = BeautifulSoup(await page.content(), 'html.parser')
-        tabla = soup.select(".tableListingTable > tbody > tr")
-        tabla.pop(0)
-        for fila in tabla:
-            id_vuelos.append(fila.select_one("td > a")['href'])
-        await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
-        await page.waitForNavigation()
+            #Select 2
+            soup = BeautifulSoup(await page.content(), 'html.parser')
+            tabla = soup.select(".tableListingTable > tbody > tr")
+            tabla.pop(0)
+            for fila in tabla:
+                id_vuelos.append(fila.select_one("td > a")['href'])
+            await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
+            await page.waitForNavigation()
+            
+            #Select 3
+            soup = BeautifulSoup(await page.content(), 'html.parser')
+            tabla = soup.select(".tableListingTable > tbody > tr")
+            tabla.pop(0)
+            for fila in tabla:
+                id_vuelos.append(fila.select_one("td > a")['href'])
+            await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
+            await page.waitForNavigation()
+            
+            #Select 4
+            soup = BeautifulSoup(await page.content(), 'html.parser')
+            tabla = soup.select(".tableListingTable > tbody > tr")
+            tabla.pop(0)
+            for fila in tabla:
+                id_vuelos.append(fila.select_one("td > a")['href'])
+            await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
+            await page.waitForNavigation()
+            
+            #Select 5
+            soup = BeautifulSoup(await page.content(), 'html.parser')
+            tabla = soup.select(".tableListingTable > tbody > tr")
+            tabla.pop(0)
+            for fila in tabla:
+                id_vuelos.append(fila.select_one("td > a")['href'])
+            await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
+            await page.waitForNavigation()
+            
+            #Select 6
+            soup = BeautifulSoup(await page.content(), 'html.parser')
+            tabla = soup.select(".tableListingTable > tbody > tr")
+            tabla.pop(0)
+            for fila in tabla:
+                id_vuelos.append(fila.select_one("td > a")['href'])
+            await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
+            await page.waitForNavigation()
 
-        #Select 2
-        soup = BeautifulSoup(await page.content(), 'html.parser')
-        tabla = soup.select(".tableListingTable > tbody > tr")
-        tabla.pop(0)
-        for fila in tabla:
-            id_vuelos.append(fila.select_one("td > a")['href'])
-        await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
-        await page.waitForNavigation()
-        
-        #Select 3
-        soup = BeautifulSoup(await page.content(), 'html.parser')
-        tabla = soup.select(".tableListingTable > tbody > tr")
-        tabla.pop(0)
-        for fila in tabla:
-            id_vuelos.append(fila.select_one("td > a")['href'])
-        await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
-        await page.waitForNavigation()
-        
-        #Select 4
-        soup = BeautifulSoup(await page.content(), 'html.parser')
-        tabla = soup.select(".tableListingTable > tbody > tr")
-        tabla.pop(0)
-        for fila in tabla:
-            id_vuelos.append(fila.select_one("td > a")['href'])
-        await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
-        await page.waitForNavigation()
-        
-        #Select 5
-        soup = BeautifulSoup(await page.content(), 'html.parser')
-        tabla = soup.select(".tableListingTable > tbody > tr")
-        tabla.pop(0)
-        for fila in tabla:
-            id_vuelos.append(fila.select_one("td > a")['href'])
-        await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
-        await page.waitForNavigation()
-        
-        #Select 6
-        soup = BeautifulSoup(await page.content(), 'html.parser')
-        tabla = soup.select(".tableListingTable > tbody > tr")
-        tabla.pop(0)
-        for fila in tabla:
-            id_vuelos.append(fila.select_one("td > a")['href'])
-        await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
-        await page.waitForNavigation()
-
-        #Select 7
-        soup = BeautifulSoup(await page.content(), 'html.parser')
-        tabla = soup.select(".tableListingTable > tbody > tr")
-        tabla.pop(0)
-        for fila in tabla:
-            id_vuelos.append(fila.select_one("td > a")['href'])
-        await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
-        await page.waitForNavigation()
-        
-        #Select 8
-        soup = BeautifulSoup(await page.content(), 'html.parser')
-        tabla = soup.select(".tableListingTable > tbody > tr")
-        tabla.pop(0)
-        for fila in tabla:
-            id_vuelos.append(fila.select_one("td > a")['href'])
-        await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
-        await page.waitForNavigation()
+            #Select 7
+            soup = BeautifulSoup(await page.content(), 'html.parser')
+            tabla = soup.select(".tableListingTable > tbody > tr")
+            tabla.pop(0)
+            for fila in tabla:
+                id_vuelos.append(fila.select_one("td > a")['href'])
+            await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
+            await page.waitForNavigation()
+            
+            #Select 8
+            soup = BeautifulSoup(await page.content(), 'html.parser')
+            tabla = soup.select(".tableListingTable > tbody > tr")
+            tabla.pop(0)
+            for fila in tabla:
+                id_vuelos.append(fila.select_one("td > a")['href'])
+            await page.evaluate('() => {document.querySelector("#flightStatusByRouteForm > select").value++; document.querySelector("#flightStatusByRouteForm").submit()}')
+            await page.waitForNavigation()
 
         for vuelo in id_vuelos:
             print(vuelo)
