8deaac4b59c20e13790ad64f50617d098a2c41b9
Cam567 <43651295+Cam567@users.noreply.github.com>
2020-04-26 14:49:25 +0200

    Twython
    Actualizacion de la API

--- Web Scrapping/tweet.py
+++ Web Scrapping/tweet.py
@@ -1,16 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 16 20:29:43 2020
-
-@author: camii
-"""
-import re 
-import tweepy 
-from tweepy import OAuthHandler 
-from textblob import TextBlob 
 from datetime import date
-
-import requests
+from twython import Twython
+# import sim
 
 data = {}
 data['opiniones'] = []
@@ -20,29 +10,20 @@ consumer_secret = 'ld3Xu6h8JMDs9fHF33is95iHG5imJkqDBEaQBbeiWTpqdshEG0'
 access_token = '805623734-svhgkst8CoLqR44X5Xmxuxdh8Lw5dMEgrM8zm9XR'
 access_token_secret = 'AWM4LzNZICFq1e6MFwsLZnaLQpDRHN4rIjreleYptwC0C'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+twitter = Twython(consumer_key, consumer_secret,
+                  access_token, access_token_secret)
 
-api = tweepy.API(auth)
-aerolineas = ['Iberia','Ryanair_ES','LATAM_ES', 'AirEuropa', 'AviancaEuropa']
+aerolineas = ['Iberia', 'Ryanair',
+              'LATAM', 'AirEuropa', 'Avianca']
 
 for j in range(len(aerolineas)):
-    it = 1
-    search = api.user_timeline(screen_name = aerolineas[j], count = 10) 
-    print("-------------------------------------")
-    # print(aerolineas[j])
-    for i in search:
-        # print(it,": ... + ", i.text)
-        it = it+1
+    query = aerolineas[j] + " atencion al cliente"
+    # search = api.user_timeline(screen_name = aerolineas[j], count = 10)
+    search = twitter.search(q=query, tweet_mode='extended')
+    for se in search['statuses']:
         data['opiniones'].append({
-            'aerolinea':aerolineas[j],
-            'texto' : i.text
-        })  
+            'nombreAerolinea': aerolineas[j],
+            'comentario': se['full_text']
+        })
+print(data)
 
-ruta_rel = "Web Scrapping\\Archivos JSON\\"
-dateStr = date.today().strftime("%Y-%m-%d")
-archivo = open(ruta_rel + "Tweets" + dateStr+ ".json", "w", encoding="utf-8")
-archivo.write(str(data['opiniones'])) 
-archivo.close()
-# requests.post('http://127.0.0.1:8000/api/twitter/opinion', json=data)
-    
