b45b9f0d4e87a5ff6ec595a5e478e9ff81a39900
CodingWill0 <38038438+CodingWill0@users.noreply.github.com>
2020-05-07 15:19:22 +0200

    Update tripadvisor3.py
    Un array de aerolíneas menor para probar el código y mejor debugging.

--- Web Scrapping/tripadvisor3.py
+++ Web Scrapping/tripadvisor3.py
@@ -2,10 +2,9 @@ from ichrome import AsyncChromeDaemon, AsyncChrome
 import asyncio
 
 async def main():
-    aerolineas = ["ANA","ASL Airlines Belgium","Aeroflot","Aerolineas Argentinas","Aeromexico","Air Austral","Air Canada","Air China LTD","Air Europa","Air France","Air India","Air Mauritius","AirBridgeCargo","Alitalia","Binter Canarias","British Airways","China Airlines","China Cargo","China Eastern Airlines","Corendon Airlines Europe","Corendon Dutch Airlines","Delta Air Lines","Ethiopian Airlines","Eurowings","Garuda Indonesia","Gol","Hainan Airlines","Iberia","Iberia Express","JAL","KLM","Kenya Airways","Korean Air","LATAM Airlines","Lauda","Lufthansa","Middle East Airlines","Qatar Airways","SA AVIANCA","SAS","Singapore Airlines","Swiftair","TUI Airways","TUIfly","Turkish Airlines","United Airlines","Vueling","Wizz Air","Wizz Air UK","Xiamen Airlines"]
+    aerolineas = ["ANA","ASL Airlines Belgium","Aeroflot","Aerolineas Argentinas","Aeromexico","Air Austral"] #Array de ejemplo de aerolíneas
     data={}
     data['comentarios'] = []
-    opiniones = []
     hook_comments_js = '''
         function(){  
             for (const readMore of document.getElementsByClassName("location-review-review-list-parts-ExpandableReview__cta--2mR2g"))
@@ -31,12 +30,14 @@ async def main():
                 async with tab():
                     await asyncio.sleep(3)
                     if(await tab.get_value('''document.querySelector(".result-title").onclick.toString()''') == None): #Caso de que no pueda encontrarse enlace a la aerolínea (por falta de resultados por ejemplo)
+                        print("Error: Aerolínea no encontrada")
                         working = False
                     else:
                         await tab.set_url("https://www.tripadvisor.es"+await tab.get_value('''document.querySelector(".result-title").onclick.toString().split("'")[3]'''))
                         await asyncio.sleep(3)
                         await tab.wait_loading(2)
                         if(await tab.get_value(hook_comments_js) == None): #Caso de que falle en la primera página de comentarios (por falta de los mismos por ejemplo)
+                            print("Error: Comentarios no encontrados")
                             working = False
                         i = 0
                         opiniones_aux = []
@@ -45,17 +46,17 @@ async def main():
                             await asyncio.sleep(3)
                             await tab.wait_loading(2)
                             if(await tab.get_value(hook_comments_js) == None): #Caso de que falle en una página de comentarios (por falta de los mismos por ejemplo)
+                                print("Error: Comentarios no encontrados")
                                 working = False
                             else:
                                 opiniones_aux.extend(await tab.get_value(hook_comments_js))
                                 await tab.js(f'document.querySelector(".pageNumbers").children[{i+1}].click()')
                         for i in opiniones_aux:
                             i['id_aerolinea']=aerolinea
-                        opiniones.extend(opiniones_aux)
-                        data['comentarios'].extend(opiniones)
-                print(len(data['comentarios'])) #Por ir viendo cuántos comentarios se van recogiendo
+                        data['comentarios'].extend(opiniones_aux)
+                print(len(data['comentarios'])) #Imprime el total de comentarios recogidos hasta el momento
             await chrome.close_browser()
-    print(data) #Imprime todos los comentarios y todo. Aquí tocaría el post.
+    print(data) #Imprime todos los comentarios. Aquí tocaría el post.
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
