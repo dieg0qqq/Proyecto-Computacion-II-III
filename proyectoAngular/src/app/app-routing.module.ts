cc38d7630106546ad6a64981ad692f2c6d2a3bf5
Camila Ordoñez Lozano <43651295+Cam567@users.noreply.github.com>
2020-05-11 15:14:36 +0200

    Pagina Buscador
    segundo tab

--- proyectoAngular/src/app/app-routing.module.ts
+++ proyectoAngular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from "./inicio/inicio.component";
+import { BuscadorComponent } from "./buscador/buscador.component";
 
 const routes: Routes = [
-  { path: '', component: InicioComponent }
+  { path: '', component: InicioComponent },
+  { path: 'buscador', component: BuscadorComponent}
 ];
 
 @NgModule({
--- proyectoAngular/src/app/app.module.ts
+++ proyectoAngular/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './header/header/header.component';
 import { FooterComponent } from './footer/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { InicioComponent } from './inicio/inicio.component';
+import { BuscadorComponent } from './buscador/buscador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    InicioComponent
+    InicioComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
