8866fb6e9765aa8798db800c37c645f20bd48174
CodingWill0 <38038438+CodingWill0@users.noreply.github.com>
2020-05-27 20:43:04 +0200

    Update por cambio de la web original
    Comentario, que el parámetro 'dirección' ha desaparecido.

--- Web Scrapping/Weather-Airports.py
+++ Web Scrapping/Weather-Airports.py
@@ -11,7 +11,6 @@ ruta_rel = "Web Scrapping\\Archivos_JSON\\"
 archivo = open(ruta_rel + "Siglas" + ".txt","r", encoding="utf-8")
 
 for line in archivo:
-    
     siglas.append(line)
     aeropuerto_sigla = line #El parámetro
     page = requests.get(f'https://www.worldweatheronline.com/v2/search.ashx?qry={aeropuerto_sigla}').json()[0]
@@ -19,23 +18,23 @@ for line in archivo:
     page = requests.get(page.url+'?day=20&tp=1')
     soup = BeautifulSoup(page.content, 'html.parser')
     print("Visto")
-
+    datos = soup.select('.col.mr-1')
     for i in range(1, 25):
         data['clima'].append({
             'idaeropuerto': aeropuerto_sigla,
             'fecha': date.today().strftime("%Y-%m-%d"),
-            'hora': soup.select_one(f'.tb_row:nth-child({i}) > .tb_cont_item:nth-child(1)').text,
-            'prevision': soup.select_one(f'.tb_row:nth-child({i}) > .tb_cont_item:nth-child(2) > img').get('alt'),
-            'temperatura': soup.select_one(f'.tb_row:nth-child({i}) > .tb_cont_item:nth-child(3)').text.replace(" °c", ""),
-            'lluvia': soup.select_one(f'.tb_row:nth-child({i}) > .tb_cont_item:nth-child(5)').text.replace(" mm", ""),
-            'nubes': soup.select_one(f'.tb_row:nth-child({i}) > .tb_cont_item:nth-child(7)').text.replace("%", ""),
-            'viento': soup.select_one(f'.tb_row:nth-child({i}) > .tb_cont_item:nth-child(8)').text.replace(" km/h",""),
-            'rafagas': soup.select_one(f'.tb_row:nth-child({i}) > .tb_cont_item:nth-child(9)').text.replace(" km/h",""),
-            'direccion': soup.select_one(f'.tb_row:nth-child({i}) > .tb_cont_item:nth-child(10) .flex_coll').getText(),
-            'humedad': soup.select_one(f'.tb_row:nth-child({i}) > .tb_cont_item:nth-child(11)').text.replace("%", ""),
-            'presion': soup.select_one(f'.tb_row:nth-child({i}) > .tb_cont_item:nth-child(12)').text.replace(" mb", ""),
+            'hora': datos[12+(11*(i-1))].text,
+            'prevision': datos[13+(11*(i-1))].select('.img-fluid')[0].get('alt'),
+            'temperatura': datos[14+(11*(i-1))].text.replace(" °c", ""),
+            'lluvia': datos[18+(11*(i-1))].text.replace(" mm", ""),
+            'nubes': datos[21+(11*(i-1))].text.replace("%", ""),
+            'viento': datos[16+(11*(i-1))].text.replace(" km/h",""),
+            'rafagas': datos[17+(11*(i-1))].text.replace(" km/h",""),
+            #'direccion' ya no existe en la web
+            'humedad': datos[20+(11*(i-1))].text.replace("%", ""),
+            'presion': datos[21+(11*(i-1))].text.replace(" mb", ""),
         })
-
+        #print(data['clima'][i-1])
 
 print(siglas)        
 print(data)
