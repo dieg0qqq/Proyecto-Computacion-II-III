2c734e741959c2d6ef8eeb258c4ab205cc4f7551
Diego Fraj <dfraj.aznarez@gmail.com>
2020-05-28 19:48:17 +0200

    comentarios trip
    eso

--- proyectoAngular/src/app/tripadvisors/tripadvisors.component.ts
+++ proyectoAngular/src/app/tripadvisors/tripadvisors.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { AerolineasService } from '../servicios/aerolineas.service';
+import { TripadvisorsService } from '../servicios/tripadvisors.service';
+
+import { tripadvisors } from '../modelos/tripadvisors';
+import { aerolineas } from '../modelos/aerolineas';
 
 @Component({
   selector: 'app-tripadvisors',
@@ -7,9 +13,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripadvisorsComponent implements OnInit {
 
-  constructor() { }
+  sql = 'http://127.0.0.1:8000/api';
+  tripadvisors: tripadvisors[];
+  aerolineas: aerolineas[];
+  arrayTripadvisor: tripadvisors[];
+
+  lista:boolean;
+  selectedLevel;
+
+  constructor(private tripadvisorService: TripadvisorsService, private aerolineasService: AerolineasService,
+    private httpClient: HttpClient) { 
+
+      httpClient.get(this.sql + '/tripadvisor/lista').subscribe((data: tripadvisors[]) => { 
+        this.tripadvisors = data;
+      });
+
+      httpClient.get(this.sql + '/aerolinea/lista').subscribe((data: aerolineas[]) => { 
+        this.aerolineas = data;
+      });
+    }
 
   ngOnInit(): void {
+    this.lista = false;
+
+  }
+
+  listaAerolineas() {
+    console.log(this.selectedLevel)
+    this.httpClient.get(this.sql + '/tripadvisorXaerolinea/' + this.selectedLevel)
+    .subscribe((data:tripadvisors[]) => { 
+      this.arrayTripadvisor = data;
+      console.log(this.arrayTripadvisor);
+      this.lista = true;
+    })
+
+
   }
 
 }
